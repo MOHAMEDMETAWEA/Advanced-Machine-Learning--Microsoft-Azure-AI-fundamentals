47d06dfa90f549ac9ad98c7456b29cb214021408
MOHAMEDMETAWEA <mm3796703@gmail.com>
2025-10-29 17:18:22 +0300

    Advanced Machine Learning+ Microsoft Azure AI fundamentals

---
+++ programs.py
@@ -0,0 +1,28 @@
+if 3 in [1,2,3]:
+    print("True")
+    
+print(3 in [1,2,3])
+
+
+L1=[1,2,3]
+
+print(3 in L1)
+
+x=input("Enter your char: ")
+
+if x in "aioue": 
+    print("char is vowel")
+else:
+    print("not vowel")
+    
+L2=['a','i','o','u','e']
+
+if x in L2:
+    print("char is vowel")
+else:
+    print("not vowel")
+
+if x=='a' or x=='i' or x=='e' or x=='o' or x=='u':
+    print("char is vowel")
+else:
+    print("not vowel")
